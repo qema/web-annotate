18c369d40c8aef54f827f37d4e599e1852367a67
Andrew Wang <azw7@cornell.edu>
2019-05-14 14:00:02 -0400

    switch from popup to click to start

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher()],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript(null, {file: "content.js"});
 });
+
+//chrome.runtime.onInstalled.addListener(function() {
+//	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//		chrome.declarativeContent.onPageChanged.addRules([{
+//            conditions: [new chrome.declarativeContent.PageStateMatcher()],
+//			actions: [new chrome.declarativeContent.ShowPageAction()]
+//		}]);
+//	});
+//});
--- popup.js
+++
@@ -1,9 +0,0 @@
-let startAnnotation = document.getElementById("startAnnotation");
-
-startAnnotation.onclick = function(elt) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id, {file: 'content.js'}
-        );
-    });
-}
